2d45871ba7d04d43a21d6ab07621bd7252c37eed
clfhhc <clfhhc@gmail.com>
2020-04-22 23:09:22 -0700

    deploy

---
+++ docs/_next/static/azUFFJyQV1LAXRbgY5Ycn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/index/[id]":[a]}}("static\u002Fchunks\u002F53fda595c96073565c5794d531fc8369f8497aba.85e6214e540657d673f8.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
